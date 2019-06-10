d29786f15101707e0e1f8f2d83842a426b44789f
okason <okason1997@hotmail.com>
2019-06-09 22:57:42 -0300

    text finished

--- disdisaster/disasterApp/urls.py
+++ disdisaster/disasterApp/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('pedirAyuda/', views.pedirAyuda, name='ask4Help'),
+	path('pedirAyuda/', views.ask4Help, name='ask4Help'),
     path('text/', views.text, name='text'),
     path('', views.index, name='index'),
     path('disasterInfo/', views.disasterInfo, name='disasterInfo'),
